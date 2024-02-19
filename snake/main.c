d7e8e7b2a127ec34a3dc122f94fe7ef99d42ed8b
Рушан Валимханов <rushanow@bk.ru>
2024-02-19 19:51:25 +0300

    Initial Commit

---
+++ snake/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  snake
+//
+//  Created by Рушан Валимханов on 19.02.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
