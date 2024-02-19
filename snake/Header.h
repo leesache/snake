2039f34bc444ef303886115e185a5c5197ce5151
leesache <159702279+leesache@users.noreply.github.com>
2024-02-20 02:06:56 +0300

    Update Header.h

--- snake/Header.h
+++ snake/Header.h
@@ -10,21 +10,21 @@
 #include "snake.h"
 
 
-void printBoard (int ** board, int rows, int cols, int snakeLength, struct Snake * Snake_segment){
+void printBoard (int ** board, int rows, int cols, int snakeLength, struct Snake * Snake_segment, int * food){
     int count = 0;
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
             if (Snake_segment[count].snake_x == i && Snake_segment[count].snake_y == j){
-                printf("=");
-                board[i][j] = 2;
+                printf("<");
                 count++;
-                
+                continue;
             }
             if (i == 0 || j == 0 || i == rows - 1 || j == cols - 1){
                 printf("*");
             }
             else if (board[i][j] == 1){
                 printf("0");
+                *food += 1;
             }
             else printf(" ");
         }
@@ -34,6 +34,29 @@ void printBoard (int ** board, int rows, int cols, int snakeLength, struct Snake
 
 int myRand(int min, int max) {
     return rand() % (max - min + 1) + min;
+}
+
+void snakeMovement (char c, struct Snake * Snake_segment, int length){
+    for (int i = 0; i < length; i++)
+        
+    switch (c) {
+        case 'w':
+            Snake_segment[i].snake_x -= 1;
+            break;
+        case 'a':
+            Snake_segment[i].snake_y -= 1;
+            break;
+        case 's':
+            Snake_segment[i].snake_x += 1;
+            break;
+        case 'd':
+            Snake_segment[i].snake_y += 1;
+            break;
+
+        default:
+            break;
+    }
+    
 }
 
 #endif /* Header_h */
